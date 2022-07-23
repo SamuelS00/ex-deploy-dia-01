0cad0dac663cd4e353bdf64be133c9839b20584d
Samuel <samuelsoutosilva14@gmail.com>
2022-07-23 13:32:08 -0300

    feat: add environment message variable

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res, _next) => {
-   res.status(200).json('Está vivo!!!')
+   res.status(200).json(process.env.MENSAGEM)
 })
 
 module.exports = app;
